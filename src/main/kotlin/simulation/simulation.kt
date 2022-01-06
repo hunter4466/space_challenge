fbb4626210cba2cfab1ec00af1fbd6d0c94a4a37
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-06 00:04:51 -0500

    Simulation: Add method to fetch text from URL and reads line by line creating Items objects and storing on ArrayList

--- src/main/kotlin/simulation/simulation.kt
+++ src/main/kotlin/simulation/simulation.kt
@@ -1,5 +1,17 @@
-package simulation
+package design.simulation
+import design.Item
+import java.net.HttpURLConnection
+import java.net.URL
 
-class Simulation {
-    
+class Simulation(private val url: String) {
+    var itemsCollection: ArrayList<Item> = arrayListOf()
+    fun loadItems() {
+        val conn = URL(url).openConnection() as HttpURLConnection
+        val responseBody = conn.inputStream.use { it.readBytes() }.toString(Charsets.UTF_8)
+        responseBody.reader().forEachLine {
+            val arr = it.split("=")
+            itemsCollection.add(Item(arr[0], arr[1] as Int))
+        }
+    }
+
 }
