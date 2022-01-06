b436dffd711668f6f24b21dcb2fe1965321d2cb0
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-06 01:32:12 -0500

    Simulation: Create runSimulation function to calculate the number of launches and the total budget of mission

--- src/main/kotlin/simulation/simulation.kt
+++ src/main/kotlin/simulation/simulation.kt
@@ -7,13 +7,13 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class Simulation(private val url: String) {
-    fun loadItems(): ArrayList<Item> {
+    private fun loadItems(): ArrayList<Item> {
         var itemsCollection: ArrayList<Item> = arrayListOf()
         val conn = URL(url).openConnection() as HttpURLConnection
         val responseBody = conn.inputStream.use { it.readBytes() }.toString(Charsets.UTF_8)
         responseBody.reader().forEachLine {
             val arr = it.split("=")
-            itemsCollection.add(Item(arr[0], arr[1] as Int))
+            itemsCollection.add(Item(arr[0], arr[1].toInt()))
         }
         return itemsCollection
     }
@@ -43,5 +43,25 @@ class Simulation(private val url: String) {
         }
         return rockets
     }
-
+    fun runSimulation(): Int {
+        val u1Rockets = loadU1()
+        val u2Rockets = loadU2()
+        var u1Budget = 0
+        var u2Budget = 0
+        for(elm in u1Rockets) {
+            fun launchRocket(rkt: U1) {
+                u1Budget += rkt.rocketCost
+                if (!rkt.launch()) launchRocket(rkt)
+            }
+            launchRocket(elm)
+        }
+        for(elm in u2Rockets) {
+            fun launchRocket(rkt: U2) {
+                u2Budget += rkt.rocketCost
+                if (!rkt.launch()) launchRocket(rkt)
+            }
+            launchRocket(elm)
+        }
+        return u1Budget.plus(u2Budget)
+    }
 }
