26db89842e84f34b97161c63377214246c079d60
hunter4466 <mario.chois.navarro@gmail.com>
2022-01-17 00:07:41 -0500

    Make requested changes in code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package design.simulation
 import design.simulation.simulation.Simulation
 
-fun main(args: Array<String>) {
+fun main() {
     val simulation1 = Simulation("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d67_phase-1/phase-1.txt")
     val simulation2 = Simulation("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d88_phase-2/phase-2.txt")
     println("Results of Phase one:")
